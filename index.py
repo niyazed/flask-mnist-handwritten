0f18de5c00a1d38c0675997131ebab71cd21512c
Niyaz B. Hashem <niyazed@outlook.com>
2019-06-19 18:51:17 +0600

    make better

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 
 from flask import Flask, render_template, request
-from scipy.misc.pilutil import imread, imresize, imsave
+from scipy.misc import imread, imresize, imsave
 import numpy as np
 import re
 import sys
